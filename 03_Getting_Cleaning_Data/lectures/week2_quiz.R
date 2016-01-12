008df076c580679f9c1c724cdc056830c0c166d0
RStancut <stancutr@gmail.com>
2016-01-12 21:33:07 +0100

    quiz incomplete

---
+++ 03_Getting_Cleaning_Data/lectures/week2_quiz.R
@@ -0,0 +1,60 @@
+if (!file.exists("data")) {
+    dir.create("data")
+}
+
+
+    #Q1
+# https://github.com/settings/applications
+# get information on your instructors repositories (hint: this is the url 
+# you want "https://api.github.com/users/jtleek/repos")
+
+# tutorial: https://github.com/hadley/httr/blob/master/demo/oauth2-github.r
+
+
+    #Q2
+library(sqldf)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, "./data/acs.csv")
+acs <- read.csv("./data/acs.csv")
+
+sqldf("select pwgtp1 from acs where AGEP < 50")
+sqldf("select count(*) from acs where AGEP < 50")
+# 10093
+
+# acs[acs$AGEP < 50, 'pwgtp1']
+# sum(acs$AGEP < 50)
+
+
+    #Q3
+unique(acs$AGEP)
+sqldf("select distinct AGEP from acs")
+
+
+    #Q4
+con = url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode = readLines(con)
+close(con)
+
+linesOfInterest = c(10, 20, 30, 100)
+for (l in linesOfInterest) {
+    print(nchar(htmlCode[l]))
+}
+
+    
+    #Q5
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+download.file(fileUrl, "./data/wksst8110.for")
+wksst <- read.fwf("./data/wksst8110.for", 
+                  skip = 4, 
+                  widths = c(11,8,4,9,4,9,4,9,4))
+head(wksst)
+sum(wksst$V4)
+
+    ### notes
+library(XML)
+library(httr)
+url <- "http://biostat.jhsph.edu/~jleek/contact.html"
+html <- GET(url)
+content2 = content(html,as="text")
+parsedHtml = htmlParse(content2,asText=TRUE)
+# xpathSApply(parsedHtml, )
