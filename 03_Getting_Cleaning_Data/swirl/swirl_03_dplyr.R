e1d34820260fb28efaf92f095829ec25794726ce
GucciTheCarpenter <stancutr@gmail.com>
2016-01-18 16:50:30 +0100

    swirl & reorder

--- 03_Getting_Cleaning_Data/swirl/swirl_03_dplyr.R
+++ 03_Getting_Cleaning_Data/swirl/swirl_03_dplyr.R
@@ -128,3 +128,99 @@ students4
 # 3  588 Sally   M     1       A     C
 # ...     ...     ...     ...
 
+# two separate tables -- 
+# one containing basic student information (id, name, and sex) 
+# and the other containing grades (id, class, midterm, final)
+student_info <- students4 %>%
+    select(id, name, sex) %>%
+    unique %>%
+    print
+#    id  name sex
+# 1 168 Brian   F
+# 3 588 Sally   M
+# 5 710  Jeff   M
+# 7 731 Roger   F
+# 9 908 Karen   M
+
+gradebook <- students4 %>%
+    select(id, class, midterm, final) %>%
+    print
+#     id class midterm final
+# 1  168     1       B     B
+# 2  168     5       A     C
+# 3  588     1       A     C
+
+passed
+#    name class final
+# 1 Brian     1     B
+# 2 Roger     2     A
+# 3 Roger     5     A
+# 4 Karen     4     A
+
+failed
+#    name class final
+# 1 Brian     5     C
+# 2 Sally     1     C
+# 3 Sally     3     C
+# 4  Jeff     2     E
+# 5  Jeff     4     C
+# 6 Karen     3     C
+
+passed <- passed %>% mutate(status = "passed")
+failed <- failed %>% mutate(status = "failed")
+bind_rows(passed, failed)
+#     name class final status
+#    (chr) (int) (chr)  (chr)
+# 1  Brian     1     B passed
+# 2  Roger     2     A passed
+# ...     ...     ...     ...
+# 5  Brian     5     C failed
+# 6  Sally     1     C failed
+# ...     ...     ...     ...
+
+# the important thing is that each row is an observation, 
+# each column is a variable, and 
+# the table contains a single observational unit. 
+# Thus, the data are tidy
+
+# http://research.collegeboard.org/programs/sat/data/cb-seniors-2013
+
+sat
+# Source: local data frame [6 x 10]
+# 
+#   score_range read_male read_fem read_total math_male math_fem math_total write_male write_fem write_total
+#         (chr)     (int)    (int)      (int)     (int)    (int)      (int)      (int)     (int)       (int)
+# 1     700-800     40151    38898      79049     74461    46040     120501      31574     39101       70675
+# 2     600-690    121950   126084     248034    162564   133954     296518     100963    125368      226331
+# 3     500-590    227141   259553     486694    233141   257678     490819     202326    247239      449565
+# 4     400-490    242554   296793     539347    204670   288696     493366     262623    302933      565556
+# 5     300-390    113568   133473     247041     82468   131025     213493     146106    144381      290487
+# 6     200-290     30728    29154      59882     18788    26562      45350      32500     24933       57433
+
+sat %>%
+    select(-contains("total")) %>%
+    gather(part_sex, count, -score_range) %>%
+    separate(part_sex, c("part", "sex")) %>%
+    print
+#    score_range  part   sex  count
+#          (chr) (chr) (chr)  (int)
+# 1      700-800  read  male  40151
+# 2      600-690  read  male 121950
+# 3      500-590  read  male 227141
+
+sat %>%
+    select(-contains("total")) %>%
+    gather(part_sex, count, -score_range) %>%
+    separate(part_sex, c("part", "sex")) %>%
+    group_by(part, sex) %>%
+    mutate(total = sum(count),
+           prop = count / total
+    ) %>% print
+#    score_range  part   sex  count  total       prop
+#          (chr) (chr) (chr)  (int)  (int)      (dbl)
+# 1      700-800  read  male  40151 776092 0.05173485
+# 2      600-690  read  male 121950 776092 0.15713343
+# ..         ...   ...   ...    ...    ...        ...
+# 9      500-590  read   fem 259553 883955 0.29362694
+# 10     400-490  read   fem 296793 883955 0.33575578
+# ..         ...   ...   ...    ...    ...        ...
